0c6a15cc8dadbf10c966a1cf13189bc532c9da7f
Edgar Rosas <gary.blurk@gmail.com>
2020-01-24 16:02:38 -0800

    Validating

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
 using Supermarket.API.Resources;
+using Supermarket.API.Extensions;
 
 namespace Supermarket.API.Controllers
 {
@@ -32,6 +33,8 @@ namespace Supermarket.API.Controllers
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] SaveCategoryResource resource)
         {
+            if (!ModelState.IsValid)
+		            return BadRequest(ModelState.GetErrorMessages());
         }
     }
 }
