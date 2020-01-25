a69b32fd68a82a854caeba2f5fb8c229e857a3b8
Edgar Rosas <gary.blurk@gmail.com>
2020-01-25 13:30:28 -0800

    POST

--- Domain/Repositories/ICategoryRepository.cs
+++ Domain/Repositories/ICategoryRepository.cs
@@ -6,7 +6,9 @@ namespace Supermarket.API.Domain.Repositories
 {
     public interface ICategoryRepository
     {
-         Task<IEnumerable<Category>> ListAsync();
-         Task AddAsync(Category category);
+        Task<IEnumerable<Category>> ListAsync();
+        Task AddAsync(Category category);
+        Task<Category> FindByIdAsync(int id);
+	    void Update(Category category);
     }
 }
